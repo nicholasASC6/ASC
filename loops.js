d03eda6a667cd67f035d25fabc03da127bc3ed3f
nicholasASC6 <nicholassingh72@gmail.com>
2019-07-10 10:08:52 -0400

    new commit

---
+++ loops.js
@@ -0,0 +1,13 @@
+for (let i = 1; i < 4; i++){console.log(i);}
+
+let arr = ['pikachu','raichu','pichu'];
+
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+};
+
+let coins = [1,2,3,4,5];
+
+for (let i = 0; i < coins.length; i++){
+    console.log(coins[i] * 100);
+}
---
+++ loops2.js
@@ -0,0 +1,14 @@
+// example code
+let str = "";
+for(let i = 0; i < 4; i++){
+    str += "*";
+    console.log(str);
+}
+
+
+// my code
+let string = "";
+for(let i = 0; i < 10; i++){
+    string += "^-+-^";
+    console.log(string);
+}
