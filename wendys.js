6626c440c1de929d43f1c6cc744b894b5598de37
nicholasASC6 <nicholassingh72@gmail.com>
2019-07-11 09:32:50 -0400

    first commit

---
+++ wendys.js
@@ -0,0 +1,17 @@
+const wallet = 1.44;
+const hungry = true;
+if(wallet >= 4.44 && hungry == true) {
+    console.log('Welcome to Wendys!');
+}
+else {
+    console.log('Sir this is a Wendys');
+}
+
+
+
+
+
+
+
+// && means and in a boolean
+// || means or in a boolean
